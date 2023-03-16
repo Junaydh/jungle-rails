1f876deffcdf2c122d6840389c354b664c20db33
Junayd H <junaydhus@gmail.com>
2023-03-15 19:30:49 -0700

    add validation to user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: true}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
+
 end
